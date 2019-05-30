c2749f5486950bc2f9c774cc1e90fa8d118b17d9
sgwilliam <sdgwills@gmail.com>
2019-05-30 08:00:39 -0600

    mid-server setup commit

--- server/server.js
+++ server/server.js
@@ -5,7 +5,11 @@ const session = require('express-session');
 const ctrl = require('./controller');
 const app = express();
 const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env;
+const path = require('path'); // Usually moved to the start of file
 
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 app.use(express.json());
 app.use(session({
   secret: SESSION_SECRET,
@@ -15,6 +19,7 @@ app.use(session({
     maxAge: 1000 * 60 * 60 * 24 * 365
   }
 }));
+app.use( express.static( `${__dirname}/../build` ) );
 
 app.get('/api/watchlist/:user_id', ctrl.readWatchlist);
 app.get('/api/stocks', ctrl.readStocks);
